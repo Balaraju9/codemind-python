8d3fd44d89a94b2562c0fcfffde7b748eb66fd5a
Balaraju9 <119840117+Balaraju9@users.noreply.github.com>
2023-05-16 19:21:52 +0530

    # Prompt the user to enter two numbers num1,num2=map(int,input().split()) # Find the maximum of the two numbers max_num = max(num1, num2)
    # Initialize the LCM as the maximum number
    lcm = max_num
    
    while True:
        # Check if both numbers are divisible by the current LCM
        if lcm % num1 == 0 and lcm % num2 == 0:
            break
    
        # Increment the LCM by the maximum number
        lcm += max_num
    
    print(lcm)

---
+++ LCM_of_two_given_numbers.py
@@ -0,0 +1,17 @@
+# Prompt the user to enter two numbers
+num1,num2=map(int,input().split())
+# Find the maximum of the two numbers
+max_num = max(num1, num2)
+
+# Initialize the LCM as the maximum number
+lcm = max_num
+
+while True:
+    # Check if both numbers are divisible by the current LCM
+    if lcm % num1 == 0 and lcm % num2 == 0:
+        break
+
+    # Increment the LCM by the maximum number
+    lcm += max_num
+
+print(lcm)

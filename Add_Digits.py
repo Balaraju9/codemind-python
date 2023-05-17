e935d8b97c6fda05b9c620f06e1b220aeb08e978
Balaraju9 <119840117+Balaraju9@users.noreply.github.com>
2023-05-17 16:47:35 +0530

    def add_digits(num):     while num >= 10:         num = sum(int(digit) for digit in str(num))     return num
    # Taking input from the user
    num = int(input())
    
    # Calculating the result
    result = add_digits(num)
    
    # Printing the result
    print(result)

--- Add_Digits.py
+++ Add_Digits.py
@@ -1,11 +1,13 @@
-n=int(input())
-s=0
-while(n!=0):
-    r=n%10
-    s=s+r
-    n=n//10
-    if(n==0 and s>=10):
-        n=s
-        s=0
-    
-print(s)
+def add_digits(num):
+    while num >= 10:
+        num = sum(int(digit) for digit in str(num))
+    return num
+
+# Taking input from the user
+num = int(input())
+
+# Calculating the result
+result = add_digits(num)
+
+# Printing the result
+print(result)

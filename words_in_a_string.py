c335f2d0fdd8fc49826aadc688d0a24432514ad6
Balaraju9 <119840117+Balaraju9@users.noreply.github.com>
2023-05-21 13:21:37 +0530

    # Read the input string = input()
    # Split the string into words
    words = string.split()
    
    # Count the number of words
    word_count = len(words)
    
    # Display the count of words
    print(word_count)

---
+++ words_in_a_string.py
@@ -0,0 +1,11 @@
+# Read the input
+string = input()
+
+# Split the string into words
+words = string.split()
+
+# Count the number of words
+word_count = len(words)
+
+# Display the count of words
+print(word_count)
